234d272bdf5f67e633c7a58d13dda55cfe03bc05
RoRep1ay <sojimmynea@gmail.com>
2023-02-16 21:32:47 +0700

    Update nvim tree to follow specified directiory

--- nvim/init.lua
+++ nvim/init.lua
@@ -19,6 +19,20 @@ require("lazy").setup("plugins")
 require('config/fold-config')
 require('config/keymap-config')
 
+local function open_nvim_tree(data)
+  -- buffer is a directory
+  local directory = vim.fn.isdirectory(data.file) == 1
+  if not directory then
+    return
+  end
+  -- change to the directory
+  vim.cmd.cd(data.file)
+  -- open the tree
+  require("nvim-tree.api").tree.open()
+end
+
+vim.api.nvim_create_autocmd({ "VimEnter" }, { callback = open_nvim_tree })
+
 vim.cmd([[silent! autocmd! filetypedetect BufRead,BufNewFile *.tf]])
 vim.cmd([[autocmd BufRead,BufNewFile *.hcl set filetype=hcl]])
 vim.cmd([[autocmd BufRead,BufNewFile .terraformrc,terraform.rc set filetype=hcl]])
